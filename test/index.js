0b2a210eddcc4df323be30b3415a496ddbbfd913
Tyler Collins <tjscollins@gmail.com>
2017-10-02 05:36:39 +0000

    Restructure tests for move from mocha to jest

--- test/index.js
+++
@@ -1,8 +0,0 @@
-const expect = require('expect');
-const sinon = require('sinon');
-const moxios = require('moxios');
-
-describe('axe-crawler', () => {
-    require('./crawler_test');
-    require('./util_test');
-});
