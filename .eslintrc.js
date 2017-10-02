ea107754d4891407ed3ff9c81ac4a3fa67acccca
Tyler Collins <tjscollins@gmail.com>
2017-10-02 05:36:09 +0000

    Configuration improvements

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,12 @@
 module.exports = {
-    "extends": "airbnb-base"
+    "extends": "airbnb-base",
+    "env": {
+        "node": true,
+        "jest": true,
+        "es6": true
+    },
+    "rules": {
+        "no-restricted-syntax": "off",
+        "no-await-in-loop": "off"
+    }
 };
