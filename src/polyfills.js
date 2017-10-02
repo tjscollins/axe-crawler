ab5dafef18524e1a30b1a646ef1bf45b691b4b18
Tyler Collins <tjscollins@gmail.com>
2017-10-02 05:37:31 +0000

    Linting

--- src/polyfills.js
+++ src/polyfills.js
@@ -7,33 +7,33 @@
 
 /**
  * polyfills - apply polyfills for needed functionality
- * 
+ *
  */
 export default function polyfills() {
-    let reduce = Function.bind.call(Function.call, Array.prototype.reduce);
-    let isEnumerable = Function.bind.call(Function.call, Object.prototype.propertyIsEnumerable);
-    let concat = Function.bind.call(Function.call, Array.prototype.concat);
-    let keys = Reflect.ownKeys;
+  const reduce = Function.bind.call(Function.call, Array.prototype.reduce);
+  const isEnumerable = Function.bind.call(Function.call, Object.prototype.propertyIsEnumerable);
+  const concat = Function.bind.call(Function.call, Array.prototype.concat);
+  const keys = Reflect.ownKeys;
 
-    Set.prototype.difference = function(setB) {
-        var difference = new Set(this);
-        for (var elem of setB) {
-            difference.delete(elem);
-        }
-        return difference;
+  Set.prototype.difference = function (setB) {
+    const difference = new Set(this);
+    for (const elem of setB) {
+      difference.delete(elem);
+    }
+    return difference;
+  };
+
+  if (!Object.values) {
+    Object.values = function values(O) {
+      return reduce(keys(O), (v, k) => concat(v, typeof k === 'string' && isEnumerable(O, k) ? [O[k]] : []), []);
     };
+  }
 
-    if (!Object.values) {
-        Object.values = function values(O) {
-            return reduce(keys(O), (v, k) => concat(v, typeof k === 'string' && isEnumerable(O, k) ? [O[k]] : []), []);
-        };
-    }
-
-    if (!Object.entries) {
-        Object.entries = function entries(O) {
-            return reduce(keys(O), (e, k) => concat(e, typeof k === 'string' && isEnumerable(O, k) ? [
-                [k, O[k]]
-            ] : []), []);
-        };
-    }
+  if (!Object.entries) {
+    Object.entries = function entries(O) {
+      return reduce(keys(O), (e, k) => concat(e, typeof k === 'string' && isEnumerable(O, k) ? [
+        [k, O[k]],
+      ] : []), []);
+    };
+  }
 }
