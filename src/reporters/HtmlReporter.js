a6cae05cb937eec8a4ab27210919a5d8f60dc309
Tyler Collins <tjscollins@gmail.com>
2017-11-27 01:06:14 +0000

    Cleanup duplicated code

--- src/reporters/HtmlReporter.js
+++ src/reporters/HtmlReporter.js
@@ -5,14 +5,15 @@ import escape from 'escape-html';
 /* --- Symbols for Private Members --- */
 
 /**
- * Private Values
+ * Private Value Symbols
  */
 const OPTIONS = Symbol('Options');
 const FILE_NAME = Symbol('Filename');
+const STYLE_SHEETS = '<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/github-markdown-css/2.8.0/github-markdown.css"><link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">';
 const CSS_STYLES = '<style> h1 { text-align: center; } ol > li { padding-bottom: 15px; font-weight: 500 } ul > li { font-weight: 400; font-size: 12px; } ol > li > span { font-weight: 400; } thead th { text-align: center; } tr { border: 1px solid lightsalmon; } tbody th, tbody td { padding: 5px; text-align: left; vertical-align: text-top; font-weight: normal; } table.summary-table { width: 100%; }</style>';
 
 /**
- * Private Methods
+ * Private Method Symbols
  */
 const INIT_HTML = Symbol('Prepare HTML File');
 const WRITE_TO_FILE = Symbol('Append an html string to the report file');
@@ -41,12 +42,15 @@ export default class HTMLReporter {
 }
 
 
+/**
+ * Private Method Implementations
+ */
 function initHTML() {
   const writeToFile = this[WRITE_TO_FILE];
 
   const titleString = `aXe Accessibility Engine Report for ${this[OPTIONS].domain}  \n${new Date().toString()}`;
 
-  writeToFile(`<!doctype html> <html lang="en"><head><title>${titleString}</title><meta name="viewport" content="width=device-width, initial-scale=1"><link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/github-markdown-css/2.8.0/github-markdown.css"><link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">${CSS_STYLES}</head><body><div class="container"><div class="row"><div class="col-xs-12">${marked(`# ${titleString}`)}`);
+  writeToFile(`<!doctype html> <html lang="en"><head><title>${titleString}</title><meta name="viewport" content="width=device-width, initial-scale=1">${STYLE_SHEETS}${CSS_STYLES}</head><body><div class="container"><div class="row"><div class="col-xs-12">${marked(`# ${titleString}`)}`);
   writeToFile(marked('This report is not a complete accessibility audit.  This report only documents those accessibility features tested by the axe-core library, and should not be considered exhaustive of the possible accessibility issues a website may have.  Use this report as a tool in a complete and comprehensive process of reviewing this site for accessibility issues.  More information regarding the features tested by the axe-core library may be found at [axe-core.org](https://axe-core.org/)'));
 
   if (this[OPTIONS].random !== 1) {
@@ -116,32 +120,49 @@ function writeNodeMessages(list, {
   /* eslint-enable no-param-reassign */
 }
 
+function printViewCounts(reports, reportType) {
+  /* eslint-disable no-param-reassign */
+  let body = '<ul>';
+  reports.forEach(({ viewPort, report: results }) => {
+    const resultsObject = JSON.parse(results);
+    body += `<li style="list-style: none; height: 15px;"><div class="col-xs-2">${viewPort}:</div><div class="col-xs-10"> ${resultsObject.length} ${reportType}</div></li>`;
+  });
+  body += '</ul>';
+  return body;
+  /* eslint-enable no-param-reassign */
+}
+
+function printResultsList({ viewPort, url, report }) {
+  const results = JSON.parse(report);
+  let html = `<h4>${viewPort.toUpperCase()}</h4><br/><ol>`;
+  if (results.length > 0) {
+    html += `${results.reduce((item, {
+      description,
+      nodes,
+      impact,
+    }) => {
+      item += `<li>${impact ? `${impact.toUpperCase()}: ` : ''}${escape(description)}<br />`;
+      item += '<span>Affected Nodes: </span><ul>';
+      item += nodes.reduce(writeNodeMessages, '');
+      item += '</ul></li>';
+      return item;
+    }, '')}</ol>`;
+    return html;
+  }
+  return '';
+}
+
 async function writeCompleteResults(testedURLs) {
   const writeToFile = this[WRITE_TO_FILE];
-  writeToFile('<div class="row"><div class="col-xs-12">');
-  writeToFile(`${marked('## Detailed List of Failing Tests')}</div></div>`);
 
+  writeToFile(`${marked('## Detailed List of Failing Tests')}`);
   await Promise.all(testedURLs.map(async ({ url }) => {
     const violations = await this[OPTIONS].db.read('violations_summary', { url });
     const violationCount = countResults(violations);
     writeToFile(marked(`### ${escape(url)} ${violationCount} violations`));
     writeToFile('<br/>');
-    await Promise.all(violations.map(({ viewPort, url, report }) => {
-      const results = JSON.parse(report);
-      if (results.length > 0) {
-        writeToFile(`<h4>${viewPort.toUpperCase()}</h4><br/><ol>${results.reduce((item, {
-          description,
-          nodes,
-          impact,
-        }) => {
-          item += `<li>${impact ? `${impact.toUpperCase()}: ` : ''}${escape(description)}<br />`;
-          item += '<span>Affected Nodes: </span><ul>';
-          item += nodes.reduce(writeNodeMessages, '');
-          item += '</ul></li>';
-          return item;
-        }, '')}`);
-      }
-    }));
+    writeToFile(printViewCounts(violations, 'failed tests'));
+    await Promise.all(violations.map(violation => writeToFile(printResultsList(violation))));
   }));
 
   writeToFile(marked('## Detailed List of Passing Tests'));
@@ -150,21 +171,7 @@ async function writeCompleteResults(testedURLs) {
     const passCount = countResults(passes);
     writeToFile(marked(`### ${escape(url)} ${passCount} passing tests`));
     writeToFile('<br/>');
-    await Promise.all(passes.map(({ viewPort, url, report }) => {
-      const results = JSON.parse(report);
-      if (results.length > 0) {
-        writeToFile(`<h4>${viewPort.toUpperCase()}</h4><br/><ol>${results.reduce((item, {
-          description,
-          nodes,
-          impact,
-        }) => {
-          item += `<li>${impact ? `${impact.toUpperCase()}: ` : ''}${escape(description)}<br />`;
-          item += '<span>Affected Nodes: </span><ul>';
-          item += nodes.reduce(writeNodeMessages, '');
-          item += '</ul></li>';
-          return item;
-        }, '')}`);
-      }
-    }));
+    writeToFile(printViewCounts(passes, 'passing tests'));
+    await Promise.all(passes.map(passed => writeToFile(printResultsList(passed))));
   }));
 }
