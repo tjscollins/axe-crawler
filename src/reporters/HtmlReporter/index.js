6e586a97a21133ca940adc06580a2a24afb91d23
Tyler Collins <tjscollins@gmail.com>
2018-01-04 11:09:50 +1000

    Add SCSS to Html Reporter, adjust src directory structure to compensate

--- src/reporters/HtmlReporter/index.js
+++ src/reporters/HtmlReporter/index.js
@@ -10,8 +10,7 @@ const FILE_NAME = Symbol('Filename');
 const TOTALS = Symbol('Total count of passing and failing tests');
 const STYLE_SHEETS =
   '<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/github-markdown-css/2.8.0/github-markdown.css"><link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">';
-const CSS_STYLES =
-  '<style> h1 { text-align: center; } ol > li { padding-bottom: 15px; font-weight: 500 } ul > li { font-weight: 400; font-size: 12px; } ol > li > span { font-weight: 400; } thead th { text-align: center; } tr { border: 1px solid lightsalmon; } tbody th, tbody td { padding: 5px; text-align: left; vertical-align: text-top; font-weight: normal; } table.summary-table { width: 100%; }</style>';
+const CSS = fs.readFileSync(`${__dirname}/../index.css`);
 
 // Private Methods
 const INIT_HTML = Symbol('Prepare HTML File');
@@ -99,9 +98,7 @@ function initHTML() {
 
   writeToFile(`<!doctype html> <html lang="en"><head><title>${
     titleString
-  }</title><meta name="viewport" content="width=device-width, initial-scale=1">${STYLE_SHEETS}${
-    CSS_STYLES
-  }</head><body><div class="container"><div class="row"><div class="col-xs-12">${marked(`# ${titleString}`)}`);
+  }</title><meta name="viewport" content="width=device-width, initial-scale=1">${STYLE_SHEETS}<style>${CSS}</style></head><body><div class="container"><div class="row"><div class="col-xs-12">${marked(`# ${titleString}`)}`);
   writeToFile(marked('This report is not a complete accessibility audit.  This report only documents those accessibility features tested by the axe-core library, and should not be considered exhaustive of the possible accessibility issues a website may have.  Use this report as a tool in a complete and comprehensive process of reviewing this site for accessibility issues.  More information regarding the features tested by the axe-core library may be found at [axe-core.org](https://axe-core.org/)'));
 
   if (this[OPTIONS].random !== 1) {
